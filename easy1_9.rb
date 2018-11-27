bd4133f7bed75bbdc4a446306c9b4620269cacd0
danachen <danaqchen@gmail.com>
2018-11-27 14:02:23 +0100

    Exercises

---
+++ easy1_9.rb
@@ -0,0 +1,27 @@
+# Iterators: True for One?
+
+# interesting
+
+
+# Mike Krahulec solution with the one-liner, I have two lines to get the same effect
+# very good way to ensure the counter is not zero and the input is not empty!
+
+def one?(input)
+  counter = 0
+  input.each do |el|
+    counter += 1 if yield(el)
+    return false if counter > 1
+  end
+  # return false if counter.zero? || input.empty?
+  # true
+  counter == 1
+end
+
+
+p one?([1, 3, 5, 6]) { |value| value.even? }    # -> true
+p one?([1, 3, 5, 7]) { |value| value.odd? }     # -> false
+p one?([2, 4, 6, 8]) { |value| value.even? }    # -> false
+p one?([1, 3, 5, 7]) { |value| value % 5 == 0 } # -> true
+p one?([1, 3, 5, 7]) { |value| true }           # -> false
+p one?([1, 3, 5, 7]) { |value| false }          # -> false
+p one?([]) { |value| true }                     # -> false
